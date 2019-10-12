4bfd5beed27a2a2ce3a0188b57fe311c1f91688a
eecsfuture <1125366597@qq.com>
2019-10-12 15:44:50 +0800

    第二章部分练习题

---
+++ chapter_2/random.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k;
+  float x, y, z;
+
+  printf("Value of i: %d\n", i);
+  printf("Value of j: %d\n", j);
+  printf("Value of k: %d\n", k);
+
+  printf("Value of x: %f\n", x);
+  printf("Value of y: %g\n", y);
+  printf("Value of z: %g\n", z);
+
+  return 0;
+}
---
+++ chapter_2/star.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void) {
+  printf("        *\n");
+  printf("       *\n");
+  printf("      *\n");
+  printf("*    *\n");
+  printf(" *  *\n");
+  printf("  *\n");
+}
